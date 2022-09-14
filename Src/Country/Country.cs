bccb82ef29608a0dff9eeca907268de125f1f31b
Alex Redfearn <alex.redfearn@dilaleredmac.mgsops.net>
2022-09-14 11:36:12 +0100

    Get counts from input

--- Src/Country/Country.cs
+++ Src/Country/Country.cs
@@ -5,6 +5,7 @@ namespace Succession
     public class Country
     {
         public Founder Founder { get; }
+        
         public Country(ICountryInput input)
         {
             Founder = input.GetFounder();
--- Src/Input/ConsoleInput.cs
+++ Src/Input/ConsoleInput.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using Internal;
 using Succession.People;
 using Succession.Util;
 
@@ -9,16 +10,27 @@ namespace Succession.Input
     public class ConsoleInput : ICountryInput
     {
         private readonly ReadOnlyCollection<string> _input;
+        private readonly int _families;
+        private readonly int _claimants;
 
         public ConsoleInput()
         {
             _input = ReadLines().AsReadOnly();
+            
+            int[] counts = GetCounts();
+            _families = counts[0];
+            _claimants = counts[1];
         }
 
         public Founder GetFounder()
         {
             string name = _input[1];
             return new Founder(name);
+        }
+
+        private int[] GetCounts()
+        {
+            return Parse.SplitNumericList(_input[0], " ");
         }
 
         private List<string> ReadLines()
